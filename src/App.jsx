a5a15db8a2fb9cc9acdeeefd1b12a2cdbf1823f3
Khye <khyeyang08@gmail.com>
2025-10-08 17:01:39 +0800

    implemented a handleLoginAttempt async function to validate login attempts - to be linked to SQL or related PLC credentials in future works

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import GT_HMI_Header from './components/GT_HMI_Header'
 import ToDo_App from './components/ToDo_App'
 
 import { Menu, X, User, Calendar, Home, Check, Plus, NavigationOff, Settings2} from 'lucide-react'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import UI_Comp_NavDrawer from './components/UI_Comp_NavDrawer'
 import MyComp_NavDrawer from './components/MyComp_NavDrawer'
 
@@ -27,15 +27,16 @@ function App() {
     { id: 4, icon: Settings2, label: 'Analytics' },
     { id: 5, icon: Check, label: 'Completed Items'},
     { id: 6, icon: Calendar, label: 'Client Meetings' },
-    { id: 7, icon: Plus, label: 'Add New Project' },
+    { id: 7, icon: Plus, label: 'Add New Project' }
   ]; 
 
   const [isOpen, setIsOpen] = useState(false);
   const [activeItemId, setActiveItemId] = useState(navItems.find(item => item.isActive)?.id || 1);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const toggleSideBar = () => {
-    setIsOpen(!isOpen);
-  };
+  const toggleSideBar = useCallback(() =>{
+    setIsOpen(prev => !prev);
+  }, []);
 
   const handleSetActive = (id) => {
     setActiveItemId(id);
@@ -45,6 +46,17 @@ function App() {
     ...item,
     isActive : item.id === activeItemId,
   }))
+
+  const handleLoginAttempt = useCallback(async (username, password) => {
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    const success = (username === 'admin' && password === '1234');
+
+    if (success) {
+      setIsLoggedIn(true);
+    }
+
+    return success;
+  }, [handleSetActive])
 
   return (
     <div>
@@ -77,7 +89,7 @@ function App() {
             <div className="flex flex-col h-[120vh] mt-8 p-6 bg-gray-800 rounded-xl shadow-inner">
               <h2 className="text-2xl font-semibold text-blue-400">Main Content Area</h2>
               <p className="text-gray-500 mt-2">Scroll down to see the full page content.</p>
-              <MyComp_SimpleLogin/>
+              <MyComp_SimpleLogin onLoginAttempt={handleLoginAttempt}/>
             </div>
           </div>
         </main>
